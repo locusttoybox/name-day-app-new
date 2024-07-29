91f57ed57ea0a8f943d8254fa0a14c7322be1349
Veronika <veronika@Veronikas-MacBook-Pro.local>
2024-07-29 17:00:07 +0300

    first commit

---
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Some Text here</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+  title: {},
+});
+
+export default HomeScreen;
